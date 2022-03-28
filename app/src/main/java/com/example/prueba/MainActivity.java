c77fb242af6d73348eb52e031460e82b150f41e1
aldel <aldelriogarcia@gmail.com>
2022-03-28 09:46:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.prueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+
+        //our app loads the site given in the URL
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://recircular.net/");
+
+        //enables Javascript
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+    }
+}
