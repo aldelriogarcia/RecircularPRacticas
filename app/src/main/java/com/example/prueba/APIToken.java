59b2c80cd7dc07ec7c13b3788d4d9e29c1057699
aldel <aldelriogarcia@gmail.com>
2022-04-19 13:55:34 +0200

    Llamada API funcional

---
+++ app/src/main/java/com/example/prueba/APIToken.java
@@ -0,0 +1,11 @@
+package com.example.prueba;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface APIToken {
+
+    @POST("androidAppToken")
+    Call<APIRespuesta> createPost(@Body String token);
+}
